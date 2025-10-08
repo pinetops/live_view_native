f4b3b710d9b1bda0f2ccdbb51e781a41d8f6f2f3
Tom Clarke <tom@u2i.com>
2025-10-08 00:40:45 -0400

    Match LV structure better

--- test/live_view_native/test/view_tree_test.exs
+++ test/live_view_native/test/view_tree_test.exs
@@ -1,6 +1,8 @@
 defmodule LiveViewNative.ViewTreeTest do
   use ExUnit.Case, async: true
 
+  import LiveViewNativeTest.ViewTree, only: [sigil_X: 2, sigil_x: 2]
+
   alias LiveViewNativeTest.ViewTree
 
   describe "find_live_views" do
@@ -8,20 +10,18 @@ defmodule LiveViewNative.ViewTreeTest do
     @too_big_session Enum.map_join(1..4432, fn _ -> "t" end)
 
     test "finds views given markup" do
-      assert ViewTree.find_live_views(
-               ViewTree.parse("""
-               <Title>top</Title>
-               <Group data-phx-session="SESSION1"
-                 id="phx-123"></Group>
-               <Group data-phx-parent-id="456"
-                   data-phx-session="SESSION2"
-                   data-phx-static="STATIC2"
-                   id="phx-456"></Group>
-               <Group data-phx-session="#{@too_big_session}"
-                 id="phx-458"></Group>
-               <Title>bottom</Title>
-               """)
-             ) == [
+      assert ViewTree.find_live_views(~x"""
+             <Title>top</Title>
+             <Group data-phx-session="SESSION1"
+               id="phx-123"></Group>
+             <Group data-phx-parent-id="456"
+                 data-phx-session="SESSION2"
+                 data-phx-static="STATIC2"
+                 id="phx-456"></Group>
+             <Group data-phx-session="#{@too_big_session}"
+               id="phx-458"></Group>
+             <Title>bottom</Title>
+             """) == [
                {"phx-123", "SESSION1", nil},
                {"phx-456", "SESSION2", "STATIC2"},
                {"phx-458", @too_big_session, nil}
@@ -31,21 +31,19 @@ defmodule LiveViewNative.ViewTreeTest do
     end
 
     test "returns main live view as first result" do
-      assert ViewTree.find_live_views(
-               ViewTree.parse("""
-               <Title>top</Title>
-               <Group data-phx-session="SESSION1"
-                 id="phx-123"></Group>
-               <Group data-phx-parent-id="456"
-                   data-phx-session="SESSION2"
-                   data-phx-static="STATIC2"
-                   id="phx-456"></Group>
-               <Group data-phx-session="SESSIONMAIN"
-                 data-phx-main="true"
-                 id="phx-458"></Group>
-               <Title>bottom</Title>
-               """)
-             ) == [
+      assert ViewTree.find_live_views(~X"""
+             <Title>top</Title>
+             <Group data-phx-session="SESSION1"
+               id="phx-123"></Group>
+             <Group data-phx-parent-id="456"
+                 data-phx-session="SESSION2"
+                 data-phx-static="STATIC2"
+                 id="phx-456"></Group>
+             <Group data-phx-session="SESSIONMAIN"
+               data-phx-main="true"
+               id="phx-458"></Group>
+             <Title>bottom</Title>
+             """) == [
                {"phx-458", "SESSIONMAIN", nil},
                {"phx-123", "SESSION1", nil},
                {"phx-456", "SESSION2", "STATIC2"}
@@ -56,35 +54,39 @@ defmodule LiveViewNative.ViewTreeTest do
   describe "replace_root_container" do
     test "replaces tag name and merges attributes" do
       container =
-        ViewTree.parse("""
+        ~X"""
         <Group id="container"
              data-phx-main="true"
              data-phx-session="session"
              data-phx-static="static"
              class="old">contents</Group>
-        """)
+        """
 
-      assert ViewTree.replace_root_container(container, :Span, %{class: "new"}) ==
+      result = ViewTree.replace_root_container(container, :Span, %{class: "new"})
+      [{tag, attrs, children}] = result
+      normalized = [{tag, Enum.sort(attrs), children}]
+
+      assert normalized ==
                [
                  {"Span",
                   [
-                    {"id", "container"},
+                    {"class", "new"},
                     {"data-phx-main", "true"},
                     {"data-phx-session", "session"},
                     {"data-phx-static", "static"},
-                    {"class", "new"}
+                    {"id", "container"}
                   ], ["contents"]}
                ]
     end
 
     test "does not overwrite reserved attributes" do
       container =
-        ViewTree.parse("""
+        ~X"""
         <Group id="container"
              data-phx-main="true"
              data-phx-session="session"
              data-phx-static="static">contents</Group>
-        """)
+        """
 
       new_attrs = %{
         "id" => "new",
@@ -93,14 +95,18 @@ defmodule LiveViewNative.ViewTreeTest do
         "data-phx-main" => "new"
       }
 
-      assert ViewTree.replace_root_container(container, :Group, new_attrs) ==
+      result = ViewTree.replace_root_container(container, :Group, new_attrs)
+      [{tag, attrs, children}] = result
+      normalized = [{tag, Enum.sort(attrs), children}]
+
+      assert normalized ==
                [
                  {"Group",
                   [
-                    {"id", "container"},
                     {"data-phx-main", "true"},
                     {"data-phx-session", "session"},
-                    {"data-phx-static", "static"}
+                    {"data-phx-static", "static"},
+                    {"id", "container"}
                   ], ["contents"]}
                ]
     end
